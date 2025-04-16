3b0102bc73a666860328041699c19fbce760428c
J-JD <jamesjohndoe@protonmail.com>
2025-04-16 16:31:31 +0200

    feat: Add a new state object: "prepared"
    It displays the message "Just Keep chopping." if some items aren't
    prepared yet. Otherwise it displays "Prep work done!".
    
    Steps:
    - Add the new state property: "prepared".
    - Add the "Effect Hook" listener: useEffect().
    - Add the "prep work" display.

--- code/3-state-events/start/src/App.jsx
+++ code/3-state-events/start/src/App.jsx
@@ -21,8 +21,10 @@ function App() {
 
     // TODO: Create recipe state
     const [recipe, setRecipe] = useState(initialRecipe);
+    
     // TODO: Add new state property
-
+    const [prepared, setPrepared] = useState(false);
+    
     // TODO: Create ingredientClick event listener
     function ingredientClick(index){
         const updatedRecipe = { ...recipe };
@@ -31,16 +33,21 @@ function App() {
     }
 
     // TODO: Add the effect hook
+    useEffect(() => {
+        setPrepared(recipe.ingredients.every(ingredient => ingredient.prepared));
+    }, [recipe]);
 
+    
     return (
         <article>
             <h1>Recipe Manager</h1>
-
             {/* TODO: Pass recipe metadata to RecipeTitle */}
             <RecipeTitle title={recipe.title} feedback={recipe.feedback}  />
             {/* TODO: Pass ingredients and event listener to IngredientList */}
             <IngredientList ingredients={recipe.ingredients} onClick={ingredientClick} />
             {/* TODO: Add the prep work display */}
+            { prepared ? <h2>Prep work done!</h2> : <h2>Just keep chooping.</h2>}
+
         </article>
     )
 }
